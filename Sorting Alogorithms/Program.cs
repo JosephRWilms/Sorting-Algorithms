a3a14fbc92f14e7f8b45e417c398b61ea32e1c08
Joseph Williams <Joseph.Williams@alpha.ca>
2019-08-28 21:38:41 -0700

    Created Data Array Base Class

--- Sorting Alogorithms/Program.cs
+++ Sorting Alogorithms/Program.cs
@@ -8,14 +8,91 @@ namespace Sorting_Alogorithms
     {
         static void Main(string[] args)
         {
-
-            String filePath = @"C:\Users\josep\source\repos\Sorting Alogorithms\Sorting Alogorithms\Data\data.csv";
-
-            DataHandler dataHandler = new DataHandler(filePath);
+            DataArray data = new DataArray(@"C:\Users\josep\source\repos\Sorting Alogorithms\Sorting Alogorithms\Data\data.csv");
+            data.displayArray();
         }
     }
 
-    class DataHandler
+    class SortAlogirthims
+    {
+        public List<int> selectionSort(List<Int32> array)
+        {
+            int smallest;
+            for (int i = 0; i < array.Count - 1; i++)
+            {
+                smallest = i;
+                for (int index = i + 1; index < array.Count; index++)
+                {
+                    if (array[index] < array[smallest])
+                    {
+                        smallest = index;
+                    }
+                }
+                array = Swap(array, i, smallest);
+            }
+
+            return array;
+        }
+
+        public static List<int> Swap(List<int> array, int first, int second)
+        {
+            int temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+            return array;
+        }
+    }
+
+    class DataArray
+    {
+        private List<int> array;
+
+        public DataArray()
+        {
+            //generateRandomData();  TODO create method
+        }
+
+        public DataArray(string filePath)
+        {
+            array = new List<int>();
+            array = loadDataFromFile(filePath);
+        }
+
+        private List<int> loadDataFromFile(string filePath)
+        {
+            var tempDataArray = new List<int>();
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    tempDataArray.Add(Int32.Parse(reader.ReadLine()));
+                }
+            }
+            return tempDataArray;
+        }
+
+        public void displayArray()
+        {
+            foreach (var line in array)
+            {
+                Console.WriteLine(line.ToString());
+            }
+        }
+
+        public List<int> getDataArray()
+        {
+            return this.array;
+        }
+
+        public void setDataArray(List<int> array)
+        {
+            this.array = array;
+        }
+    }
+
+
+
+    /*class DataHandler
     {
         private List<int> dataArray;
 
@@ -23,12 +100,16 @@ namespace Sorting_Alogorithms
         {
             dataArray = loadData(filePath);
             displayArray(dataArray);
+
+            SortAlogirthims sorter = new SortAlogirthims();
+
+            dataArray = 
         }
 
         private List<int> loadData(string filePath)
         {
             var tempDataArray = new List<int>();
-            using(var reader = new StreamReader(filePath))
+            using (var reader = new StreamReader(filePath))
             {
                 while (!reader.EndOfStream)
                 {
@@ -40,9 +121,40 @@ namespace Sorting_Alogorithms
 
         public static void displayArray(List<int> array)
         {
-            foreach(var line in array) {
+            foreach (var line in array)
+            {
                 Console.WriteLine(line.ToString());
             }
         }
-    }
+
+        class SortAlogirthims
+        {
+            public static List<int> selectionSort(List<Int32> array)
+            {
+                int smallest;
+                for (int i = 0; i < array.Count - 1; i++)
+                {
+                    smallest = i;
+                    for (int index = i + 1; index < array.Count; index++)
+                    {
+                        if (array[index] < array[smallest])
+                        {
+                            smallest = index;
+                        }
+                    }
+                    array = Swap(array, i, smallest);
+                }
+
+                return array;
+            }
+
+            public static List<int> Swap(List<int> array, int first, int second)
+            {
+                int temp = array[first];
+                array[first] = array[second];
+                array[second] = temp;
+                return array;
+            }
+        }
+    }*/
 }
